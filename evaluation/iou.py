f81e416bc2665dc4a64d89b3bb7de2c06ff93052
juyebshin <sjy9803@nate.com>
2022-06-07 20:55:07 +0900

    Add Chamfer distance

--- evaluation/iou.py
+++ evaluation/iou.py
@@ -16,3 +16,51 @@ def get_batch_iou(pred_map, gt_map):
             intersects.append(intersect)
             unions.append(union)
     return torch.tensor(intersects), torch.tensor(unions)
+
+def get_batch_cd(pred_positions: torch.Tensor, gt_vectors: list, masks: torch.Tensor, patch_size: list):
+    # pred_positions: [b, N, 3]
+    # gt_vectors: [b] list of [instance] list of dict
+    # masks: [b, N, 1]
+
+    cdist_p_list = []
+    cdist_l_list = []
+    with torch.no_grad():
+        patch_size = torch.tensor([patch_size[1], patch_size[0]]).cuda()
+        for pred_position, gt_vector, mask in zip(pred_positions, gt_vectors, masks):
+            # pred_position: [N, 3]
+            # gt_vector: [instance] list of dict
+            mask = mask.squeeze(-1) # [N]
+            position_valid = pred_position[..., :-1] * patch_size # de-normalize, [N, 2]
+            position_valid = position_valid[mask == 1] # [M, 2] x, y c
+            pts_list = []
+            for ins, vector in enumerate(gt_vector): # dict
+                pts, pts_num, type = vector['pts'], vector['pts_num'], vector['type']
+                pts = pts[:pts_num] # [p, 2] array
+                [pts_list.append(pt) for pt in pts]
+            
+            gt_position = torch.tensor(pts_list).float().cuda()
+
+            if len(gt_position) > 0 and len(position_valid) > 0:
+                # compute chamfer distance # [N, P] shaped tensor
+                cdist_p = torch.cdist(position_valid, gt_position) # [M, P] prediction to label
+                cdist_l = cdist_p.T # [P, M] label to prediction
+                # nearest ground truth vectors
+                cdist_p_mean = torch.mean(cdist_p.min(dim=-1).values) # [N,]
+                cdist_l_mean = torch.mean(cdist_l.min(dim=-1).values) # [P,]
+            else:
+                cdist_p_mean = torch.tensor(-1.0).float().cuda()
+                cdist_l_mean = torch.tensor(-1.0).float().cuda()
+            
+            cdist_p_list.append(cdist_p_mean)
+            cdist_l_list.append(cdist_l_mean)
+        
+        batch_cdist_p = torch.stack(cdist_p_list) # [b,]
+        batch_cdist_l = torch.stack(cdist_l_list) # [b,]
+        mask_p = batch_cdist_p != -1.0
+        mask_l = batch_cdist_l != -1.0
+
+        batch_cdist_p_mean = (batch_cdist_p*mask_p).sum(dim=0) / mask_p.sum(dim=0)
+        batch_cdist_l_mean = (batch_cdist_l*mask_l).sum(dim=0) / mask_l.sum(dim=0)
+    return batch_cdist_p_mean, batch_cdist_l_mean
+
+
