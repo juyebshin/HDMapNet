7d3bb7b04dba36fedfbdb97ab440e2aa6966a6a7
juyebshin <sjy9803@nate.com>
2022-10-17 13:34:42 +0900

    Add visualization video by scene

--- img2video.py
+++ img2video.py
@@ -22,6 +22,7 @@ def parse_args():
                         help='FPS of output video. default: 30fps')
     parser.add_argument('--w', type=int, default=400) # 800 200
     parser.add_argument('--h', type=int, default=200) # 600 196
+    parser.add_argument('--scene', action='store_true')
     args = parser.parse_args()
     if args.output_dir == None:
         args.output_dir = args.input_dir
@@ -72,24 +73,51 @@ if __name__ == '__main__':
     args = parse_args()
 
     vs = VideoStreamer(args.input_dir, img_glob=args.img_glob)
-    outname = os.path.join(args.output_dir, 'video.mp4')
-    # out = cv2.VideoWriter(outname, cv2.VideoWriter_fourcc(*'DIVX'), 30)
-    out = cv2.VideoWriter(filename=outname, fourcc=cv2.VideoWriter_fourcc(*'DIVX'), fps=args.fps, frameSize=(args.w,args.h))
 
-    idx = 0
-    while True:
-        image, file_name, status = vs.next_frame()
-        if status is False:
-            break
-        if out.isOpened(): # only for front camera
-            # print('index: {}'.format(idx))
-            # print('image: {}'.format(file_name))
-            # print('image size: {}'.format(image.shape))
-            out.write(image)
+    if not args.scene:
+        outname = os.path.join(args.output_dir, 'video.mp4')
+        # out = cv2.VideoWriter(outname, cv2.VideoWriter_fourcc(*'DIVX'), 30)
+        out = cv2.VideoWriter(filename=outname, fourcc=cv2.VideoWriter_fourcc(*'DIVX'), fps=args.fps, frameSize=(args.w,args.h))
 
+        idx = 0
+        while True:
+            image, file_name, status = vs.next_frame()
+            if status is False:
+                break
+            if out.isOpened(): # only for front camera
+                # print('index: {}'.format(idx))
+                # print('image: {}'.format(file_name))
+                # print('image size: {}'.format(image.shape))
+                out.write(image)
     
-    if out.isOpened():
-        out.release()
-        print('video saved as: {}'.format(outname))
-    else:
-        print('Failed to save video')
+        if out.isOpened():
+            out.release()
+            print('video saved as: {}'.format(outname))
+        else:
+            print('Failed to save video')
+    else: # by scene
+        scene_list = []
+        out = None
+        outname = None
+        while True:
+            image, file_name, status = vs.next_frame()
+            if status is False:
+                if len(scene_list) > 0 and out is not None and outname is not None:
+                    if out.isOpened():
+                        out.release()
+                        print('last video saved as: {}'.format(outname))
+                print('All videos saved!')
+                break
+            scene_name = file_name.split('/')[-1].split('_')[0]
+            if scene_name not in scene_list:
+                if len(scene_list) > 0 and out is not None:
+                    if out.isOpened():
+                        out.release()
+                        print('video saved as: {}'.format(outname))
+                        out = None
+                scene_list.append(scene_name)
+                outname = os.path.join(args.output_dir, f'{scene_name}.mp4')
+                out = cv2.VideoWriter(filename=outname, fourcc=cv2.VideoWriter_fourcc(*'DIVX'), fps=args.fps, frameSize=(args.w,args.h))
+            
+            if out is not None and out.isOpened():
+                out.write(image)
