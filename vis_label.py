d8fbf0c6f10ace74aa8317cd4579dea1c27e9cbd
juyebshin <sjy9803@nate.com>
2024-04-03 18:34:17 +0900

    add is_train args

--- vis_label.py
+++ vis_label.py
@@ -11,19 +11,19 @@ from data.utils import get_proj_mat, perspective
 from data.image import denormalize_img
 
 
-def vis_label(dataroot, version, xbound, ybound, sample_dist):
+def vis_label(dataroot, version, xbound, ybound, sample_dist, is_train):
     data_conf = {
         'image_size': (900, 1600),
         'xbound': xbound,
         'ybound': ybound,
-        'sample_dist': args.sample_dist, # 1.5
+        'sample_dist': sample_dist, # 1.5
     }
 
     color_map = np.random.randint(0, 256, (256, 3))
     color_map[0] = np.array([0, 0, 0])
     colors_plt = ['tab:red', 'tab:blue', 'tab:green']
 
-    dataset = HDMapNetDataset(version=version, dataroot=dataroot, data_conf=data_conf, is_train=False)
+    dataset = HDMapNetDataset(version=version, dataroot=dataroot, data_conf=data_conf, is_train=is_train)
     gt_path = os.path.join(dataroot, 'GT')
     if not os.path.exists(gt_path):
         os.mkdir(gt_path)
@@ -42,10 +42,10 @@ def vis_label(dataroot, version, xbound, ybound, sample_dist):
 
         if not os.path.exists(base_path):
             os.mkdir(base_path)
-        plt.figure()
-        plt.xlim(xbound[0], xbound[1])
-        plt.ylim(ybound[0], ybound[1])
-        plt.axis('off')
+        # plt.figure()
+        # plt.xlim(xbound[0], xbound[1])
+        # plt.ylim(ybound[0], ybound[1])
+        # plt.axis('off')
         num_vectors = 0
         for vector in vectors:
             pts, pts_num, line_type = vector['pts'], vector['pts_num'], vector['type']
@@ -53,39 +53,39 @@ def vis_label(dataroot, version, xbound, ybound, sample_dist):
             x = np.array([pt[0] for pt in pts])
             y = np.array([pt[1] for pt in pts])
             # plt.quiver(x[:-1], y[:-1], x[1:] - x[:-1], y[1:] - y[:-1], scale_units='xy', angles='xy', scale=1, color=colors_plt[line_type])
-            plt.scatter(x, y, s=1.5, c=colors_plt[line_type])
-            plt.plot(x, y, linewidth=2.0, color=colors_plt[line_type], alpha=0.7)
+            # plt.scatter(x, y, s=1.5, c=colors_plt[line_type])
+            # plt.plot(x, y, linewidth=2.0, color=colors_plt[line_type], alpha=0.7)
             num_vectors += pts_num
         num_vectors_list.append([base_path, num_vectors])
 
-        plt.imshow(car_img, extent=[-1.5, 1.5, -1.2, 1.2])
+        # plt.imshow(car_img, extent=[-1.5, 1.5, -1.2, 1.2])
 
-        map_path = os.path.join(base_path, 'MAP.png')
-        plt.savefig(map_path, bbox_inches='tight', pad_inches=0, dpi=1200)
-        plt.close()
+        # map_path = os.path.join(base_path, 'MAP.png')
+        # plt.savefig(map_path, bbox_inches='tight', pad_inches=0, dpi=1200)
+        # plt.close()
 
-        major_xticks = np.linspace(int(xbound[0]), int(xbound[1]), int((xbound[1]-xbound[0])/(8*xbound[2])+1))
-        major_yticks = np.linspace(int(ybound[0]), int(ybound[1]), int((ybound[1]-ybound[0])/(8*ybound[2])+1))
-        plt.figure()
-        plt.xlim(xbound[0], xbound[1])
-        plt.ylim(ybound[0], ybound[1])
-        plt.axis('off')
-        plt.xticks(major_xticks, fontsize=2)
-        plt.yticks(major_yticks, fontsize=2)
-        plt.grid(True)
+        # major_xticks = np.linspace(int(xbound[0]), int(xbound[1]), int((xbound[1]-xbound[0])/(8*xbound[2])+1))
+        # major_yticks = np.linspace(int(ybound[0]), int(ybound[1]), int((ybound[1]-ybound[0])/(8*ybound[2])+1))
+        # plt.figure()
+        # plt.xlim(xbound[0], xbound[1])
+        # plt.ylim(ybound[0], ybound[1])
+        # plt.axis('off')
+        # plt.xticks(major_xticks, fontsize=2)
+        # plt.yticks(major_yticks, fontsize=2)
+        # plt.grid(True)
         
-        for vector in vectors:
-            pts, pts_num, line_type = vector['pts'], vector['pts_num'], vector['type']
-            pts = pts[:pts_num]
-            x = np.array([pt[0] for pt in pts])
-            y = np.array([pt[1] for pt in pts])
-            plt.scatter(x, y, s=0.1, c=colors_plt[line_type])
+        # for vector in vectors:
+        #     pts, pts_num, line_type = vector['pts'], vector['pts_num'], vector['type']
+        #     pts = pts[:pts_num]
+        #     x = np.array([pt[0] for pt in pts])
+        #     y = np.array([pt[1] for pt in pts])
+        #     plt.scatter(x, y, s=0.1, c=colors_plt[line_type])
 
-        plt.imshow(car_img, extent=[-1.5, 1.5, -1.2, 1.2])
+        # plt.imshow(car_img, extent=[-1.5, 1.5, -1.2, 1.2])
 
-        map_path = os.path.join(base_path, 'VERTEX.png')
-        plt.savefig(map_path, bbox_inches='tight', pad_inches=0, dpi=1200)
-        plt.close()
+        # map_path = os.path.join(base_path, 'VERTEX.png')
+        # plt.savefig(map_path, bbox_inches='tight', pad_inches=0, dpi=1200)
+        # plt.close()
 
         # for img, intrin, rot, tran, cam in zip(imgs, intrins, rots, trans, CAMS):
         #     img = denormalize_img(img)
@@ -113,7 +113,10 @@ def vis_label(dataroot, version, xbound, ybound, sample_dist):
         #     plt.savefig(cam_path, bbox_inches='tight', pad_inches=0, dpi=400)
         #     plt.close()
     
-    with open('num_vectors_val_long_range.csv', 'w') as f:
+    prefix = 'train' if is_train else 'val'
+    if xbound[1] > 30:
+        prefix += '_long_range'
+    with open(f'num_vectors_{prefix}.csv', 'w') as f:
         print('saving number of vectors list to csv...')
         write = csv.writer(f)
         write.writerows(num_vectors_list)
@@ -126,6 +129,7 @@ if __name__ == '__main__':
     parser.add_argument("--xbound", nargs=3, type=float, default=[-30.0, 30.0, 0.15])
     parser.add_argument("--ybound", nargs=3, type=float, default=[-15.0, 15.0, 0.15])
     parser.add_argument("--sample_dist", type=float, default=1.5)
+    parser.add_argument("--is_train", action='store_true')
     args = parser.parse_args()
 
-    vis_label(args.dataroot, args.version, args.xbound, args.ybound, args.sample_dist)
+    vis_label(args.dataroot, args.version, args.xbound, args.ybound, args.sample_dist, args.is_train)
